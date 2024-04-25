e49da3f401edd2227a16ca3c11fba007f6a50528
nikisathe <nikitasathe1734@gmail.com>
2024-04-25 20:48:14 +0530

    five commit

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const path = require('path');
-const dotenv = require('dotenv');
+const express = require("express");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const path = require("path");
+const dotenv = require("dotenv");
 
-dotenv.config(); // Load environment variables from .env file
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -14,33 +14,37 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const college_connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
 });
 
 college_connection.connect((err) => {
   if (err) {
-    console.log('error');
+    console.log("error");
   } else {
-    console.log('Connected to the college database!');
+    console.log("Connected to the college database!");
   }
 });
 
-app.post('/check', (req, res) => {
+app.post("/check", (req, res) => {
   const college_code = req.body.college_code;
-  const sql = 'SELECT * FROM college_list WHERE college_code = ?';
+  const sql = "SELECT * FROM college_list WHERE college_code = ?";
   college_connection.query(sql, [college_code], (err, results) => {
     if (err) {
-      res.status(500).json({ error: 'Internal server error' });
+      res.status(500).json({ error: "Internal server error" });
       return;
     }
     if (results.length === 0) {
-      res.status(404).json({ error: 'College code not found!' });
+      res.status(404).json({ error: "College code not found!" });
     } else {
       if (results[0].college_code === college_code) {
-        res.status(200).json({ message: 'College code found successfully!', data: results[0] });
+        res
+          .status(200)
+          .json({
+            message: "College code found successfully!",
+            data: results[0],
+          });
       } else {
-        res.status(403).json({ error: 'College code is not valid!' });
+        res.status(403).json({ error: "College code is not valid!" });
       }
     }
   });
